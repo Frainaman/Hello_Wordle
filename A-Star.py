6e381b6b56c792ffc397b0ab62cd2a5cddbb15ee
Chris <chrisexpo67@gmail.com>
2024-12-30 12:05:03 +0100

    Created first draft of feedback function and implemented priority queue

--- A-Star.py
+++ A-Star.py
@@ -1,1 +1,27 @@
-print ("hello world")
+import heapq
+
+#Priority Queue
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+    def empty(self):
+        return len(self.elements) == 0
+
+#Funzione di calcolo del feedback
+def feedback_function(guess, solution):
+    feedback = []
+    for g, s in zip(guess, solution):
+        if g == s:
+            feedback.append("green")
+        elif g in solution:
+            feedback.append("yellow")
+        else:
+            feedback.append("grey")
+    return feedback
